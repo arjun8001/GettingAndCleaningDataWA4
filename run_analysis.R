764d6727b91653f296b571ea1668d9e912548322
arjun8001 <gupta.arjun.rajendra@gmail.com>
2021-01-27 15:53:39 +0530

    solution

--- run_analysis.R
+++ run_analysis.R
@@ -1,41 +1,43 @@
-library(dplyr)
+# load libraries
+library(dplyr) 
+# set dataset directory
+setwd("UCI HAR Dataset")
 
-# read train data
-X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
-Y_train <- read.table("./UCI HAR Dataset/train/Y_train.txt")
-Sub_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 
-# read test data
-X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
-Y_test <- read.table("./UCI HAR Dataset/test/Y_test.txt")
-Sub_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+#read test, train data, features, activities
+x_test          <- read.table("./test/X_test.txt")
+read_x_train    <- read.table("./train/X_train.txt")
+y_test          <- read.table("./test/Y_test.txt") 
+read_y_train    <- read.table("./train/Y_train.txt") 
+sub_test        <- read.table("./test/subject_test.txt")
+sub_train       <- read.table("./train/subject_train.txt")
+features        <- read.table("./features.txt")
+activity_labels <- read.table("./activity_labels.txt") 
 
-# read data description
-variable_names <- read.table("./UCI HAR Dataset/features.txt")
 
-# read activity labels
-activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
+# binding data using rbind
+binding_x_total   <- rbind(read_x_train, x_test)
+binding_y_total   <- rbind(read_y_train, y_test) 
+train_subject_overall         <- rbind(sub_train, sub_test) 
 
-# 1. Merges the training and the test sets to create one data set.
-X_total <- rbind(X_train, X_test)
-Y_total <- rbind(Y_train, Y_test)
-Sub_total <- rbind(Sub_train, Sub_test)
+#extracting mean and std deviation
+sel_features <- variable_names[grep(".*mean\\(\\)|std\\(\\)", features[,2], ignore.case = FALSE),]
+binding_x_total      <- binding_x_total[,sel_features[,1]]
 
-# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
-selected_var <- variable_names[grep("mean\\(\\)|std\\(\\)",variable_names[,2]),]
-X_total <- X_total[,selected_var[,1]]
+# extracting column names
+colnames(binding_x_total)   <- sel_features[,2]
+colnames(binding_y_total)   <- "activity"
+colnames(train_subject_overall) <- "subject"
 
-# 3. Uses descriptive activity names to name the activities in the data set
-colnames(Y_total) <- "activity"
-Y_total$activitylabel <- factor(Y_total$activity, labels = as.character(activity_labels[,2]))
-activitylabel <- Y_total[,-1]
+# making final table
+total_xy <- cbind(train_subject_overall, binding_y_total, binding_x_total)
 
-# 4. Appropriately labels the data set with descriptive variable names.
-colnames(X_total) <- variable_names[selected_var[,1],2]
+# turn activities & subjects into factors 
+total_xy$activity <- factor(total_xy$activity, levels = activity_labels[,1], labels = activity_labels[,2]) 
+total_xy$subject  <- as.factor(total_xy$subject) 
 
-# 5. From the data set in step 4, creates a second, independent tidy data set with the average
-# of each variable for each activity and each subject.
-colnames(Sub_total) <- "subject"
-total <- cbind(X_total, activitylabel, Sub_total)
-total_mean <- total %>% group_by(activitylabel, subject) %>% summarize_each(funs(mean))
-write.table(total_mean, file = "./UCI HAR Dataset/tidydata.txt", row.names = FALSE, col.names = TRUE)
+
+
+total_mean <- total_xy %>% group_by(activity, subject) %>% summarize_all(funs(mean)) 
+
+write.table(total_mean, file = "./tidydata.txt", row.names = FALSE, col.names = TRUE) 
